b2b4f2f232c9e903b416b5b3fb539b293b25c563
vpg1 <malihtorovich@mail.ru>
2022-09-15 21:22:07 +0300

    Lab1: implement MyCustomCollection

---
+++ Lab1/Lab1/Collections/MyCustomCollection.cs
@@ -0,0 +1,233 @@
+using System.Collections.Specialized;
+
+namespace Lab1.Collections;
+
+using Interfaces;
+
+public class MyCustomCollection<T> : ICustomCollection<T> where T : IComparable
+{
+    class Node
+    {
+        public T value;
+        public Node? next = null;
+        public Node? prev = null;
+    }
+
+    private Node? head = null;
+    private Node? tail = null;
+    private Node? cursor = null;
+    private int count = 0;
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index >= Count)
+            {
+                throw new IndexOutOfRangeException("out of range");
+            }
+            
+            var temp = head;
+            
+            while (count != 0)
+            {
+                --count;
+                head = head.next;
+            }
+
+            return head.value;
+        }
+        set
+        {
+            
+            if (index >= Count)
+            {
+                throw new IndexOutOfRangeException("out of range");
+            }
+            
+            var temp = head;
+            
+            while (count != 0)
+            {
+                --count;
+                head = head.next;
+            }
+
+            head.value = value;
+
+        }
+    }
+    
+    public void Reset()
+    {
+        cursor = head;
+    }
+
+    public void Next()
+    {
+        if (cursor is null) throw new IndexOutOfRangeException("zero elements in the collection");
+        
+        cursor = cursor.next ?? throw new IndexOutOfRangeException("cursor on the last element of the collection");
+    }
+
+    public T Current()
+    {
+        if (cursor is null) throw new NullReferenceException("zero elements in the collection");
+        
+        return cursor.value;
+    }
+
+    public int Count
+    {
+        get
+        {
+            return count;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (tail == null) // если в коллекции ноль элементов
+        {
+            head = new Node {value = item, next = null, prev = null};
+            tail = head;
+            cursor = head;
+            return;
+        }
+        
+        // проверяем нет ли в коллекции такого же элемента
+        var temp = head;
+        while (temp != null)
+        {
+            if (temp.value.CompareTo(item) == 0)
+            {
+                throw new InvalidOperationException("this item is already in the collection");
+            }
+
+            temp = temp.next;
+        }
+        
+        // добавляем в конец
+        tail.next = new Node {value = item, next = null, prev = tail};
+        tail = tail.next;
+    }
+
+    public void Remove(T item)
+    {
+        #nullable disable
+        
+        var temp = head;
+        while (temp != null)
+        {
+            if (temp.value.CompareTo(item) == 0)
+            {
+                if (temp.prev == null && temp.next == null)
+                {
+                    head = null;
+                    tail = null;
+                    
+                    Reset();
+                }
+                else if (temp.prev == null)
+                {
+                    head = temp.next;
+                    temp.next.prev = temp.prev;
+                    
+                    if (temp.value.CompareTo(cursor.value) == 0)
+                    {
+                        Reset();
+                    }
+                }
+                else if (temp.next == null)
+                {
+                    tail = temp.prev;
+                    temp.prev.next = temp.next;
+                    
+                    if (temp.value.CompareTo(cursor.value) == 0)
+                    {
+                        Reset();
+                    }
+                }
+                else
+                {
+                    temp.prev.next = temp.next;
+                    temp.next.prev = temp.prev;
+                    
+                    if (temp.value.CompareTo(cursor.value) == 0)
+                    {
+                        Reset();
+                    }
+                }
+
+                return;
+            }
+            
+            temp = temp.next;
+        }
+        
+        #nullable restore
+    }
+
+    public T RemoveCurrent()
+    {
+        
+        if (cursor == null) throw new InvalidOperationException("zero elements in Cursor");
+        
+        var cursorVal = cursor.value; 
+        
+        #nullable disable
+
+        if (cursor.prev == null && cursor.next == null)
+        {
+            head = null;
+            tail = null;
+                    
+            Reset();
+        }
+        else if (cursor.prev == null)
+        {
+            head = cursor.next;
+            cursor.next.prev = cursor.prev;
+                    
+            if (cursor.value.CompareTo(cursor.value) == 0)
+            {
+                Reset();
+            }
+        }
+        else if (cursor.next == null)
+        {
+            tail = cursor.prev;
+            cursor.prev.next = cursor.next;
+                    
+            if (cursor.value.CompareTo(cursor.value) == 0)
+            {
+                Reset();
+            }
+        }
+        else
+        {
+            cursor.prev.next = cursor.next;
+            cursor.next.prev = cursor.prev;
+                    
+            if (cursor.value.CompareTo(cursor.value) == 0)
+            {
+                Reset();
+            }
+        }
+        
+        #nullable restore
+
+        return cursorVal;
+    }
+
+
+    public void Print()
+    {
+        var temp = head;
+        while (temp != null)
+        {
+            Console.WriteLine(temp.value);
+            temp = temp.next;
+        }
+    }
+}
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,60 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Lab1.Collections;
+
 Console.WriteLine("Hello, World!");
+
+// MyCustomCollection<int> list = new();
+//
+// list.Add(12);
+// list.Add(13);
+// list.Add(14);
+// list.Add(15);
+// list.Add(16);
+//
+// list.Remove(16);
+// list.Remove(13);
+// list.Remove(12);
+// list.Remove(14);
+// list.Remove(15);
+//
+// list.Add(51);
+// list.Add(13);
+// list.Add(14);
+// list.Add(15);
+// list.Add(16);
+//
+//
+//
+// list.Print();
+//
+//
+// Console.WriteLine();
+//
+//
+// Console.WriteLine(list.Current());
+//
+// list.Next();
+//
+// Console.WriteLine(list.Current());
+//
+// list.Remove(13);
+//
+// Console.WriteLine(list.Current());
+//
+// list.Remove(51);
+//
+//
+// Console.WriteLine(list.Current());
+// Console.WriteLine();
+//
+// list.Print();
+//
+// Console.WriteLine();
+//
+// list.Remove(14);
+// list.Remove(15);
+// list.Remove(16);
+//
+//
+// Console.WriteLine(list.Current());
