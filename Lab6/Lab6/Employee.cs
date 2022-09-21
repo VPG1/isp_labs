19337816d897eb169bcf153abe826346eb78d07b
Kirill <malihtorovich@mail.ru>
2022-09-21 23:47:43 +0300

    Lab6: add Employee class

---
+++ Lab6/Lab6/Employee.cs
@@ -0,0 +1,16 @@
+namespace Lab6;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int Payment { get; set; }
+    public bool OnSickLeave { get; set; }
+
+    public Employee(string name, int payment, bool onSickLeave)
+    {
+        Name = name;
+        Payment = payment;
+        OnSickLeave = onSickLeave;
+    }
+
+}
