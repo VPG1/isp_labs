89cb9315cea735bc8b110cd6a17bbbbfb3d88ba4
Kirill <malihtorovich@mail.ru>
2022-09-21 23:54:49 +0300

    Lab6: add file service library

---
+++ Lab6/FileServiceLibrary/FileService.cs
@@ -0,0 +1,16 @@
+﻿using Lab6;
+
+namespace FileServiceLibrary;
+
+public class FileService : IFileService<Employee>
+{
+    public IEnumerable<Employee> ReadFile(string fileName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SaveData(IEnumerable<Employee> data, string fileName)
+    {
+        throw new NotImplementedException();
+    }
+}
